d7be1ca600a1ef6572f1a6fa6848c05a7f9f37fc
Bizley <pb@human-device.com>
2015-01-19 15:04:42 +0100

    Examples

---
+++ example_controller.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @author Paweł Bizley Brzozowski
+ * @version 1.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * AjaxDropDown form controller example
+ * @see https://github.com/bizley-code/Yii-AjaxDropDown
+ * 
+ * See README file for configuration and usage examples.
+ * 
+ * In this example form data is sent with POST method.
+ * AjaxDropDown field attribute name is 'names' and it is used to collect all 
+ * selected names of the CModel2 objects used as data source in the widget.
+ * Before validation we check if selected records ids are in the database.
+ * In case validation returns errors the selected data is still present 
+ * underneath the widget.
+ */
+class FormController extends Controller
+{
+    public function actionAdd()
+    {
+        $model = new CModel();
+        $dropDownData = array();
+        $data = Yii::app()->request->getPost('CModel');
+
+        if ($data) {
+
+            $model->setAttributes($data);
+
+            if (count($model->names)) {
+                foreach ($model->names as $name) {
+                    $tmp = CModel2::model()->findByPk((int)$name);
+                    if ($tmp) {
+                        $dropDownData[] = [
+                            'id'    => $tmp->id,
+                            'mark'  => $tmp->type == 'special' ? 1 : 0,
+                            'value' => CHtml::encode($tmp->name),
+                        ];
+                    }
+                }
+            }
+
+            if ($model->validate()) {
+
+                if ($model->save()) {
+
+                    if (count($model->names)) {
+                        foreach ($model->names as $name) {
+                            if (is_numeric($name)) {
+                                $oldName = CModel2::model()->findByAttributes(['model_id' => $model->id, 'name_id' => (int)$name]);
+                                if (!$oldName) {
+                                    $model2           = new Cmodel2;
+                                    $model2->model_id = $model->id;
+                                    $model2->name_id  = (int)$name;
+                                    $model2->save();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        $this->render('add', array(
+            'model' => $model, 
+            'dropDownData' => $dropDownData
+        ));
+    }
+}
---
+++ example_data_source.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @author Paweł Bizley Brzozowski
+ * @version 1.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * AjaxDropDown data source example
+ * @see https://github.com/bizley-code/Yii-AjaxDropDown
+ * 
+ * See README file for configuration and usage examples.
+ * 
+ * In this example data is the series of CModel objects.
+ * When no query is given (nothing was typed in the view's input text field) 
+ * all the records with status = 1 are sent (per page).
+ * If query is given then records are filtered to match '%query%' SQL like 
+ * (and status = 1).
+ * Every record with 'type' = 'special' is flagged with 'mark' = 1.
+ */
+class ExampleDataSourceController extends Controller
+{
+    public function actionData()
+    {
+        $results = array(
+            'data' => array(),
+            'page' => 1,
+            'total' => 0
+        );
+        $query   = Yii::app()->request->getPost('query');
+        $page    = Yii::app()->request->getPost('page');
+        
+        $currentPage = 0;
+        if (!empty($page) && is_numeric($page) && $page > 0) {
+            $currentPage = $page - 1;
+        }
+
+        if (empty($query)) {
+            $dataProvider = new CActiveDataProvider('CModel', array(
+                'criteria'   => array(
+                    'condition' => 'status = 1',
+                    'order'     => 'name',
+                ),
+                'pagination' => array('pageSize' => 10),
+            ));
+        }
+        else {
+            $query = CHtml::encode(strip_tags($query));
+            
+            $dataProvider = new CActiveDataProvider('CModel', array(
+                'criteria'   => array(
+                    'condition' => 'name LIKE :name AND status = 1',
+                    'params'    => array(':name' => '%' . $query . '%'),
+                    'order'     => 'name',
+                ),
+                'pagination' => array('pageSize' => 10),
+            ));
+        }
+
+        $dataProvider->getPagination()->setCurrentPage($currentPage);
+
+        foreach ($dataProvider->getData() as $data) {
+            $results['data'][] = array(
+                'id'    => $data->id,
+                'mark'  => $data->type == 'special' ? 1 : 0,
+                'value' => CHtml::encode($data->name),
+            );
+        }
+
+        $results['page']  = $dataProvider->getPagination()->currentPage + 1;
+        $results['total'] = $dataProvider->getPagination()->pageCount;
+
+        echo CJSON::encode($results);
+        Yii::app()->end();
+    }
+}
